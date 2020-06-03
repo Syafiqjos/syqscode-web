8a243d9c53fef92ad498e5bb1cfeafbcbf5c8c2e
Syafiqjos <syafiqjos@gmail.com>
2020-06-03 16:24:58 +0700

    Welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <div id="master" align="center">
             <div class="position-middle-center" width="100%">
                 <div>
-                    <h1 v-on:click="anim_encode()" v-on:mouseover="anim_go()" v-on:mouseleave="anim_lv()" class="logo-title logo-title-big" style="margin:0px;"> [@{{title}}@{{ch}}] </h1>
+                    <h1 v-on:click="anim_encode()" class="logo-title logo-title-big" style="margin:0px;"> [@{{title}}] </h1>
                 </div>
                 <div class="main-links" align="center">
                     <span class="sub-links"><a href="blog/gta">Technologies</a></span>
@@ -23,6 +23,122 @@
                     <span class="sub-links"><a href="blog/san">CTFs</a></span>
                     <span class="sub-links"><a href="blog/san">Tutorial</a></span>
                     <span class="sub-links"><a href="blog/san">About Me</a></span>
+                </div>
+            </div>
+            <div class="welcome-desc"><h2 style="color: white;font-family: 'SquareFont';letter-spacing:4px;">Welcome to The Art of Code</h2></div>
+            <div class="wrapped" >
+                <div class="wrapped-blog" width="90%">
+                    <div class="page" style="background-image: radial-gradient(circle,black,black,black ,rgba(10,10,10,0.72) 0%),url({{asset('/lib/glitch.jpg')}});background-size:cover;">
+                        <div style="width:32%; display: inline-block;">
+                            <div style="height: 100%;">
+                                <img class="cover" src="{{asset('/lib/glitch.jpg')}}" align="left" />
+                            </div>
+                        </div>
+                        <div style="width:68%; display: inline-block;">
+                            <h1 class="title">How to make</h1>
+                            <div class="paragraph">
+                                it is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret.
+                            </div>
+                            <div class="divider">
+                                <div class="label-parent">
+                                    <h2 align="left">Label :
+                                        <span class="label">
+                                            <a href="google.com">Technologies</a>
+                                        </span>
+                                        ,
+                                        <span class="label">
+                                            <a href="google.com">Tutorial</a>
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="readmore" style="position:relative;bottom:0px;">
+                                    <h2 align="right">Read More</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page" style="background-image: radial-gradient(circle,black,black,black ,rgba(10,10,10,0.72) 0%),url({{asset('/lib/fbi.jpg')}});background-size:cover;">
+                        <div style="width:32%; display: inline-block;">
+                            <div style="height: 100%;">
+                                <img class="cover" src="{{asset('/lib/fbi.jpg')}}" align="left" />
+                            </div>
+                        </div>
+                        <div style="width:68%; display: inline-block;">
+                            <h1 class="title" >How to make s something useful</h1>
+                            <div class="paragraph">
+                                it is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smiling
+                            </div>
+                            <div class="divider">
+                                <div class="label-parent">
+                                    <h2 align="left">Label :
+                                        <span class="label">
+                                            <a href="google.com">Technologies</a>
+                                        </span>
+                                        ,
+                                        <span class="label">
+                                            <a href="google.com">Tutorial</a>
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="readmore" style="position:relative;bottom:0px;">
+                                    <h2 align="right">Read More</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page" style="background-image: radial-gradient(circle,black,black,black ,rgba(10,10,10,0.72) 0%),url({{asset('/lib/stars.jpg')}});background-size:cover;">
+                        <div style="width:32%; display: inline-block;">
+                            <div style="height: 100%;">
+                                <img class="cover" src="{{asset('/lib/stars.jpg')}}" align="left" />
+                            </div>
+                        </div>
+                        <div style="width:68%; display: inline-block;">
+                            <h1 class="title" >How to make s something useful</h1>
+                            <div class="paragraph" align="justify">
+                                it is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smiling      it is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smilingit is hard to make some predictions. your life is not good as you think. Maybe its not to late to make something useful before you regret. in the past when you keep smiling. Whenever wherever i keep smiling
+                            </div>
+                            <div class="divider">
+                                <div class="label-parent">
+                                    <h2 align="left">Label :
+                                        <span class="label">
+                                            <a href="google.com">Technologies</a>
+                                        </span>
+                                        ,
+                                        <span class="label">
+                                            <a href="google.com">Tutorial</a>
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="readmore" style="position:relative;bottom:0px;">
+                                    <h2 align="right">Read More</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrapped-panel" width="10%">
+                    <div class="segment">
+                        <h2 >Popular Post</h2>
+                        <div class="subsegment">
+                            <div>
+                                <h3><img src="/lib/fbi.jpg" width="80%" ></img>
+                                    Download GTA san andreas dengan menggunakan kincir angin yang sangat besar
+                                </h3>
+                            </div>
+                        </div>
+                        <div class="subsegment">
+                            <h3><img src="/lib/fbi.jpg" width="80%" ></img>
+                                Download GTA V
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="segment">
+                        <h2>Webpage</h2>
+                        <div class="subsegment">
+                            <h3>Homepage</h3>
+                            <h3>About Me</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,16 +151,9 @@
                 next_title : '',
                 isloading : false,
                 loading : 0,
-                ch : ''
+                scroll_val : 0
             },
             methods : {
-                anim_go(){
-                    // this.isloading = true;
-                },
-                anim_lv(){
-                    // this.isloading = false;
-                    // this.ch = '';
-                },
                 anim_encode(){
                     this.loading = 0;
                     this.isloading = true;
